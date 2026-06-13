192eb170533fea0ffc5689f92cb7a939201caa7f
satyamshorrf <satyamshorrf@gmail.com>
2026-06-14 03:52:47 +0530

    Data Types ✅

--- PythonLectures/my_code.py
+++ PythonLectures/my_code.py
@@ -5,3 +5,22 @@ PI = 3.14
 print ("My name is", name)
 print ("I am", age, "years old")
 print ("The value of PI is", PI)
+
+
+
+print (type(name))
+
+print (type(age))
+
+print (type(PI))
+
+
+num = 3
+
+isPrime = True
+
+print (type(num))
+print (type(isPrime))
+
+Hey = None
+print (type(Hey))
